694dde003316f7d01802262e3d9d7caeb7a9a23b
Luke D'Alfonso <pokemonmegaman@yahoo.com>
2016-11-12 16:05:58 +0000

    Added node static code

---
+++ app.js
@@ -0,0 +1,8 @@
+var express = require("express");
+var app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+app.listen(process.env.PORT || 8080, function(){
+    console.log("Listening on " + process.env.PORT || 8080);
+});
